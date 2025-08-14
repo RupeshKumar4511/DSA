f2a4b24df7733cb553ea2a0e21ee6afad34bec41
RupeshKumar4511 <kumarrn4511@gmail.com>
2025-08-14 15:23:29 +0530

    RadixSort updated

---
+++ SortingAlgorithms/RadixSort.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class RadixSort {
+private static int largest(int[] arr){
+        int max = arr[0];
+        for(int i = 1 ; i<arr.length;i++ ){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    private static void countSort(int[] arr,int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        
+        for(int i = 0 ;i<n;i++){
+            count[(arr[i]/exp)%10]++;
+        }
+        for(int j = 1 ;j <10;j++){
+            count[j] = count[j]+ count[j-1];
+        }
+        System.out.println("Updated count Array for "+exp+ "="+Arrays.toString(count));
+
+        for(int k = n-1 ;k>=0;k--){
+            output[count[(arr[k]/exp)%10]-1]= arr[k];
+            count[(arr[k]/exp)%10]--;
+        }
+        System.out.println("Sorted Output array for "+exp+"="+Arrays.toString(output));
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    public static void radixSort(int[] arr){
+        int max = largest(arr);
+        // do count sort for every digit place
+        for(int exp = 1 ;max/exp>0;exp*=10){
+            countSort(arr,exp);     
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {29,83,471,36,91,8};
+        System.out.println("Original Array : "+ Arrays.toString(arr));
+        radixSort(arr);
+        System.out.println("Sorted Array : "+Arrays.toString(arr));
+    }
+}
