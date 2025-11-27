93900be6fd49e7a49b3206daa5af82e46a5652e3
RupeshKumar4511 <kumarrn4511@gmail.com>
2025-11-27 19:40:08 +0530

    subset sum problem updated

--- ADA/subset_sum.py
+++ ADA/subset_sum.py
@@ -11,8 +11,34 @@ def subset_sum(ind,target,arr):
     return (take or not_take)
 
 
+def subset_sum_tabulation(arr, target):
+    n = len(arr)
+    dp = [[False] * (target + 1) for _ in range(n)]
+
+    # sum 0 (when picking nothing)
+    for i in range(n):
+        dp[i][0] = True
+
+    # First element initialization
+    if arr[0] <= target:
+        dp[0][arr[0]] = True
+
+    # Fill the table
+    for i in range(1, n):
+        for k in range(1, target + 1):
+            not_take = dp[i - 1][k]
+            take = False        
+            if k >= arr[i]:
+                take = dp[i - 1][k - arr[i]]
+            dp[i][k] = take or not_take
+
+    return dp[n-1][target]
+
+            
+    
 arr = [1,2,3,4,5]
 ind = len(arr)-1
 target = 7 
 result = subset_sum(ind , target,arr)
+result = subset_sum_tabulation (arr,target)
 print(result)
