d9cff159f017b0b2d8a77cde699bc2deac149743
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-28 18:56:53 +0530

    Questions updated

--- basics/Array/Practice.java
+++ basics/Array/Practice.java
@@ -1551,6 +1551,47 @@
 
 // }
 
+
+// square root of a no 
+// class Solution {
+//     // This function returns the floor value of the square root of a number
+//     public int mySqrt(int x) {
+//         // Handle small numbers directly
+//         if (x < 2) return x;
+
+//         // Initialize binary search range
+//         int left = 1, right = x / 2, ans = 0;
+
+//         // Perform binary search
+//         while (left <= right) {
+//             // Find middle point
+//             long mid = left + (right - left) / 2;
+
+//             // Check if mid*mid is less than or equal to x
+//             if (mid * mid <= x) {
+//                 // Store mid as potential answer
+//                 ans = (int) mid;
+//                 // Move to right half
+//                 left = (int) mid + 1;
+//             } else {
+//                 // Move to left half
+//                 right = (int) mid - 1;
+//             }
+//         }
+
+//         // Return final answer
+//         return ans;
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Solution s = new Solution();
+//         System.out.println(s.mySqrt(8));
+//     }
+// }
+
+
 // Q. Given an array arr which consists of non-negative integers and an integers
 // m, you can split the array into m non-empty continuous subarrays.
 // hint : we need to find the smallest of largest sum in the subarray.
